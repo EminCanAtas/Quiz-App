2de8601805adea4362fed30146e394d948616ba8
Emin Can Atas <emin@saveas.com.tr>
2021-11-15 13:42:43 +0300

    Seeder separated from each other

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            'name' => 'Emin Can Ataş',
+            'email' => 'ghostreconjaga@gmail.com',
+            'type' => 'admin',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10)
+        ]);
+
+        User::factory(5)->create();
+    }
+}
